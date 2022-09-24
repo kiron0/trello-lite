814fbcf89f646cb1fc37919cba369d2127e09452
Toufiq Hasan  Kiron <toufiqhasankiron0@gmail.com>
2022-09-25 02:16:54 +0600

    added all

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./Components/Main";
+
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1 style={{ marginBottom: "20px", color: '#000' }}>Trello Lite</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/dummyData.js
@@ -0,0 +1,48 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dummyData = [
+  {
+    id: uuidv4(),
+    title: "📝To Do",
+    tasks: [
+      {
+        id: uuidv4(),
+        title: "Studying React",
+      },
+      {
+        id: uuidv4(),
+        title: "Study on youtube",
+      },
+      {
+        id: uuidv4(),
+        title: "stroll",
+      },
+    ],
+  },
+  {
+    id: uuidv4(),
+    title: "🚀In Progress",
+    tasks: [
+      {
+        id: uuidv4(),
+        title: "coding",
+      },
+      {
+        id: uuidv4(),
+        title: "job change",
+      },
+    ],
+  },
+  {
+    id: uuidv4(),
+    title: "🌳Completed",
+    tasks: [
+      {
+        id: uuidv4(),
+        title: "reading",
+      },
+    ],
+  },
+];
+
+export default dummyData;
